35c54c447261c966515684d6eed3915f6f2fa9da
dani herrera <ctrl.alt.d@gmail.com>
2023-05-30 07:51:55 +0200

    Mostrar formulari hut per defecte (#23)
    Perfecte. Entès.

--- src/demoandtest/createDemoData.py
+++ src/demoandtest/createDemoData.py
@@ -1,27 +1,6 @@
-from formularis.models import Pregunta, Resposta, RespostaPregunta
-
 
 def run():
     """
     Crea dades de demo
     """
-    RespostaPregunta.objects.all().delete()
-    Pregunta.objects.all().delete()
-    Resposta.objects.all().delete()
-    pregunta1 = Pregunta.objects.create(text="Hi ha porta giratòria?")
-    pregunta2 = Pregunta.objects.create(text="Hi ha rampa d'accés?")
-    resposta1 = Resposta.objects.create(text="Sí")
-    resposta2 = Resposta.objects.create(text="No")
-    resposta3 = Resposta.objects.create(text="No cal rampa, no hi ha escales")
-
-    RespostaPregunta.objects.create(
-        pregunta=pregunta1, resposta=resposta1, ordre=1)
-    RespostaPregunta.objects.create(
-        pregunta=pregunta1, resposta=resposta2, ordre=2)
-
-    RespostaPregunta.objects.create(
-        pregunta=pregunta2, resposta=resposta1, ordre=1)
-    RespostaPregunta.objects.create(
-        pregunta=pregunta2, resposta=resposta2, ordre=2)
-    RespostaPregunta.objects.create(
-        pregunta=pregunta2, resposta=resposta3, ordre=3)
+    pass
--- src/formularis/crispyhelpers.py
+++ src/formularis/crispyhelpers.py
@@ -1,21 +1,45 @@
+from typing import List
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import (
     Layout, Fieldset, Submit
 )
+from QandA.models import AgrupacioPreguntes
+
+from espais.models import TipusEspai
+
+
+def _calcula_camps(agrupacio: AgrupacioPreguntes) -> List[str]:
+    return [
+        preguntadinstipusespai.pregunta.codi
+        for preguntadinstipusespai
+        in agrupacio.preguntadinstipusespai_set.all()
+    ]
+
+
+def _calcula_layout(agrupacio: AgrupacioPreguntes) -> Fieldset:
+    camps = _calcula_camps(agrupacio)
+    return Fieldset(
+        agrupacio.text_ca,
+        *camps,
+    )
 
 
 class PreguntaFormHelper(FormHelper):
     """
     Layout for pregunta formset
     """
-    def __init__(self, fields, *args, **kwargs):
+
+    def __init__(self, tipusespai: TipusEspai, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.form_method = 'post'
+
+        fieldsets = [
+            _calcula_layout(agrupacio)
+            for agrupacio in tipusespai.agrupaciopreguntes_set.all()
+        ]
+
         self.layout = Layout(
-            Fieldset(
-                'Prova de concepte',
-                *fields,
-            ),
+            *fieldsets,
             Submit('submit', 'Submit', css_class='button white'),
         )
         self.render_required_fields = True
